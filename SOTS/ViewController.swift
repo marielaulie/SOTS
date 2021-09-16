9b259bb36e0746cb8bcb619e61423b9ef783e2da
marielaulie <54407324+marielaulie@users.noreply.github.com>
2021-09-16 15:37:58 +0200

    Laget app

---
+++ SOTS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SOTS
+//
+//  Created by Mariel Aulie Hinderaker on 16/09/2021.
+//  Copyright © 2021 Mariel Aulie Hinderaker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
